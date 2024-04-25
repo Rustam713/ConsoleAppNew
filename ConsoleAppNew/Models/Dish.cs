013473750d6f4e04b4e3251009d71756965bfb9d
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-04-25 20:39:18 +0600

    added model Dish

--- ConsoleAppNew/Models/Dish.cs
+++ ConsoleAppNew/Models/Dish.cs
@@ -8,5 +8,21 @@ namespace ConsoleAppNew.Models
 {
     internal class Dish
     {
+        public int DishId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int RestaurantId { get; set; }
+        public int IngredientsId { get; set; }
+
+
+        public Dish(int dishId, string name, decimal price, int restaurant, int ingredients)
+        {
+            DishId = dishId;
+            Name = name;
+            RestaurantId = restaurant;
+            IngredientsId = ingredients;
+            Price = price;
+
+        }
     }
 }
