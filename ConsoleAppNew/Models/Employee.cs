14f31bc8ad6311b8b9dbd23deee410fd1ca949ef
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-04-25 20:40:01 +0600

    added model Employee

--- ConsoleAppNew/Models/Employee.cs
+++ ConsoleAppNew/Models/Employee.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppNew.Models
 {
-    internal class Employee
+    public class Employee
     {
+        private static int nextId = 1;
+
+        public int Employee_Code { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Position { get; set; }
+        public string WorkSchedule { get; set; }
+        public int RestaurantID { get; set; }
+
+        public Employee(int employee_code)
+        {
+            Employee_Code = employee_code;
+        }
+
+        public Employee(int employee_code, string name, string position, decimal salary, string workschedule, int restaurant)
+        {
+
+            Employee_Code = employee_code;
+            Name = name;
+            Salary = salary;
+            Position = position;
+            WorkSchedule = workschedule;
+            RestaurantID = restaurant;
+        }
     }
 }
+
